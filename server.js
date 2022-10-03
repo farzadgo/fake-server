82e2c492760ba14e231118e017b70e79361512d4
farzadgo <farzadesign@gmail.com>
2022-10-03 12:14:48 +0200

    back to js server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-import { db } from './db'
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(db)
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3200
 
